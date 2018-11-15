2c05ca429bcb5e41a2b5e41aaad7615ba867d42b
Sergio jr <devsergiojr@gmail.com>
2018-11-14 22:46:15 -0300

    sw

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
+if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(function(reg){
+        console.log("Yes, it did.");
+    }).catch(function(err) {
+        console.log("No it didn't. This happened: ", err)
+    });
+}
+
 function initMap() {
     
     let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 7,
         center: {lat: -9.28, lng: -36.05},
     });
-
+    
     const url = 'https://catalogodeservico.com.br/saude/hospitais/';
     fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            let services = data;
-
-            return services.map(service => {
-                let latitude = parseFloat(service.y_coordinate, 6);
-                let longitude = parseFloat(service.x_coordinate, 6);
-
-                let latLng = { lat: latitude, lng: longitude };
-
-                let marker = new google.maps.Marker({
-                    position: latLng,
-                    map: map
-                })
-            });
+    .then(response => response.json())
+    .then(data => {
+        let services = data;
+        
+        return services.map(service => {
+            let latitude = parseFloat(service.y_coordinate, 6);
+            let longitude = parseFloat(service.x_coordinate, 6);
+            
+            let latLng = { lat: latitude, lng: longitude };
+            
+            let marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            })
         });
+    });
 };
--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,92 @@
-var cacheName = 'services-catalog-1.0.2';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/styles.css',
-  '/js/vendor/jquery.min.js',
-  '/js/vendor/materialize.min.js',
-  '/js/distance.js',
-  '/js/main.js',
-  '/js/script.js',
-  '/partials/emergencia.html',
-  '/partials/saude.html',
-  '/partials/servicos.html',
-  '/partials/urgencia.html',
-  '/partials/serviceContent.html',
-  '/icons/MaterialIcons-Regular.ttf',
-  '/img/48.png',
-  '/img/72.png',
-  '/img/128.png',
-  '/img/144.png',
-  '/img/168.png',
-  '/img/192.png',
-  '/img/512.png',
-  ];
+var CACHE_NAME = 'services-catalog-1.0.1';
+var urlsToCache = [
+'/',
+'/index.html',
+'/css/styles.css',
+'/js/vendor/jquery.min.js',
+'/js/vendor/materialize.min.js',
+'/js/distance.js',
+'/js/main.js',
+'/js/script.js',
+'/partials/emergencia.html',
+'/partials/saude.html',
+'/partials/servicos.html',
+'/partials/urgencia.html',
+'/partials/serviceContent.html',
+'/icons/MaterialIcons-Regular.ttf',
+'/img/48.png',
+'/img/72.png',
+'/img/128.png',
+'/img/144.png',
+'/img/168.png',
+'/img/192.png',
+'/img/512.png',
+];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-    );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+    caches.match(event.request)
       .then(response => {
-      return response || fetch(event.request);
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', event => {
+
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
