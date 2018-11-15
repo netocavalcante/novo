6c046df9a21e1ece3a9f960d128e8fc4d5439604
Sergio jr <devsergiojr@gmail.com>
2018-11-15 16:25:55 -0300

    sw funcionando?

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
         .register('service-worker.js')
-        .then(function() { console.log('Service Worker Registered'); });
+        .then(() => console.log('Service Worker Registered'));
     }
 }
 
